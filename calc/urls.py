f701a3ad3e7d416f28ade99e373ec6e7c436b05c
vikram <vikrambamba85@gmail.com>
2020-06-01 02:31:37 +0530

    added header link and other menu pages

--- calc/urls.py
+++ calc/urls.py
@@ -1,6 +1,8 @@
 from django.urls import  path 
 from . import views 
+from .views import AboutPageView
 
 urlpatterns = [
+    path('about/', AboutPageView.as_view(), name='about'),
     path('',views.home, name = 'home' )
 ]
--- calc/views.py
+++ calc/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
 
 def home(request):
     return render (request, 'home.html')
 
+class AboutPageView(TemplateView): # new
+    template_name = 'about.html'
